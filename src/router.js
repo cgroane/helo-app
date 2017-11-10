d438a8ec2497386275e89370cfefb3871ae3749e
Bryce Hull <bahull@live.com>
2017-11-10 13:02:28 -0600

    Completed landing page

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import LandingPage from "./components/LandingPage/LandingPage";
+// import Dashboard from "./components/Dashboard/Dashboard";
+// import Profile from "./components/Profile/Profile";
+import Search from "./components/Search/Search";
+
+export default (
+  <Switch>
+    <Route path="/auth" component={LandingPage} />
+    {/* <Route exact path="/" component={Dashboard} />
+    <Route path="/profile" component={Profile} /> */}
+    <Route path="/search" component={Search} />
+  </Switch>
+);
